279e6ae2298bd729d653e901a97367684b86f9bb
Szymon Sobania (szymsob434) <szymsob434@student.polsl.pl>
2017-02-18 19:56:43 +0100

    troche zmieniona wersja z mastera

--- LatexEditor/LatexPolyline.cs
+++ LatexEditor/LatexPolyline.cs
@@ -12,29 +12,18 @@ namespace LatexEditor
 {
     class LatexPolyline : LatexLine
     {
-       //private List<LatexPoint> pointList;
-        //private Dictionary<Ellipse, LatexPoint> pointList;
-        private List<Tuple<LatexPoint, Ellipse>> pointList;
+        private List<LatexPoint> pointList;
         private List<Line> lines;
 
         public LatexPolyline()
         {
-            pointList = new List<Tuple<LatexPoint, Ellipse>>();
+            pointList = new List<LatexPoint>();
             lines = new List<Line>();
         }
 
         public void AddPoint(LatexPoint latexPoint, Canvas mainCanvas)
         {
-            Ellipse elipse = new Ellipse();
-            elipse.Width = 6;
-            elipse.Height = 6;
-            elipse.Stroke = new SolidColorBrush(Colors.Black);
-            elipse.Fill = new SolidColorBrush(Colors.Black);
-            elipse.StrokeThickness = 6;
-            Canvas.SetLeft(elipse, latexPoint.X);
-            Canvas.SetTop(elipse, latexPoint.Y);
-            pointList.Add(Tuple.Create(latexPoint, elipse));
-            mainCanvas.Children.Add(elipse);        
+            pointList.Add(latexPoint);            
         }
 
         internal void Draw(Canvas mainCanvas)
@@ -44,40 +33,45 @@ namespace LatexEditor
                  mainCanvas.Children.Remove(line);
             }
 
+            foreach (LatexPoint point in pointList) // Draw points
+            {
+                point.Draw(mainCanvas);
+            }
+
             for (int i =0; i<pointList.Count-1; i++) // Draw new lines
             {
                 Line line = new Line();
-                line.X1 = pointList[i].Item1.X + 3;
-                line.Y1 = pointList[i].Item1.Y + 3;
-                line.X2 = pointList[i+1].Item1.X + 3;
-                line.Y2 = pointList[i+1].Item1.Y + 3;
+                line.X1 = pointList[i].X + 3;
+                line.Y1 = pointList[i].Y + 3;
+                line.X2 = pointList[i+1].X + 3;
+                line.Y2 = pointList[i+1].Y + 3;
                 line.Stroke = new SolidColorBrush(Colors.Black);
                 line.StrokeThickness = 1;
                 mainCanvas.Children.Add(line);
                 lines.Add(line);
+
             }        
         }
 
         internal void UpdatePoint(Ellipse draggedPoint, LatexPoint latexPoint)
         {
-            foreach (Tuple<LatexPoint, Ellipse> pair in pointList) 
+            foreach (LatexPoint point in pointList) 
             {
-                if (pair.Item2 == draggedPoint)
+                if (point.ellipse == draggedPoint)
                 {
-                    pair.Item1.SetPosition(latexPoint);
-                    Canvas.SetLeft(pair.Item2, latexPoint.X);
-                    Canvas.SetTop(pair.Item2, latexPoint.Y);
+                    point.SetPosition(latexPoint);
+                    Canvas.SetLeft(point.ellipse, latexPoint.X);
+                    Canvas.SetTop(point.ellipse, latexPoint.Y);
                     break;
                 }
-            }
-           
+            }           
         }
 
-        public bool Contain(Ellipse elipse)
+        public override bool Contain(Ellipse elipse)
         {
-            foreach (Tuple<LatexPoint, Ellipse> pair in pointList)
+            foreach (LatexPoint point in pointList)
             {
-                if (pair.Item2 == elipse)
+                if (point.ellipse == elipse)
                     return true;
             }
             return false;
